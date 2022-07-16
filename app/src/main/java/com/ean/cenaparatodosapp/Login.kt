e618c869a49587ef226a0a4263725a3fac569266
Marcela Avila <marcela.avila@quipux.com>
2022-07-16 16:20:57 -0500

    Realizar ajustes en login

--- app/src/main/java/com/ean/cenaparatodosapp/Login.kt
+++ app/src/main/java/com/ean/cenaparatodosapp/Login.kt
@@ -2,14 +2,16 @@ package com.ean.cenaparatodosapp
 
 import android.content.ContentValues
 import android.content.Intent
-import androidx.appcompat.app.AppCompatActivity
+import android.graphics.Paint
 import android.os.Bundle
 import android.util.Log
 import android.widget.Toast
+import androidx.appcompat.app.AppCompatActivity
 import com.ean.cenaparatodosapp.databinding.ActivityLoginBinding
 import com.google.firebase.auth.FirebaseAuth
 import com.google.firebase.auth.ktx.auth
 import com.google.firebase.ktx.Firebase
+
 
 class Login : AppCompatActivity() {
 
